9ac202d2d9ec601a94ca2ba33aa9fbf9348f87fb
Python_Pioneer <89539411+Tonyjr7@users.noreply.github.com>
2025-03-03 15:32:06 +0100

    Update settings.py

--- bidapp/settings.py
+++ bidapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bidapi-ce67.onrender.com']
+ALLOWED_HOSTS = ['bidloggingapi.onrender.com']
 
 
 # Application definition
